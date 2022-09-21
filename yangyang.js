0b58c3181e9e60ccbdaff8a6036e836bcf8d3a4b
Cl0udsoar <zhouyunxiang@vip.qq.com>
2022-09-21 11:11:04 +0800

    Create yangyang.js
    重写

---
+++ yangyang.js
@@ -0,0 +1,2 @@
+$request.body<{"err_code":0,"err_msg":"","data":{"map_md5":["046ef1bab26e5b9bfe2473ded237b572","046ef1bab26e5b9bfe2473ded237b572"],"map_seed":[3042065807,873076577,3555997730,3291121329]}}>
+$done({body});
