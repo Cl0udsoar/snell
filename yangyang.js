612379f03cfdefd8cde3da1538004ded45200201
Cl0udsoar <zhouyunxiang@vip.qq.com>
2022-09-21 11:32:47 +0800

    Update yangyang.js
    更新写法

--- yangyang.js
+++ yangyang.js
@@ -1,2 +1,6 @@
-$request.body<{"err_code":0,"err_msg":"","data":{"map_md5":["046ef1bab26e5b9bfe2473ded237b572","046ef1bab26e5b9bfe2473ded237b572"],"map_seed":[3042065807,873076577,3555997730,3291121329]}}>
+var body = $response.body;
+var obj = JSON.parse(`{"err_code":0,"err_msg":"","data":{"map_md5":["046ef1bab26e5b9bfe2473ded237b572","046ef1bab26e5b9bfe2473ded237b572"],"map_seed":[3042065807,873076577,3555997730,3291121329]}}`);
+
+body = JSON.stringify(obj);
+
 $done({body});
