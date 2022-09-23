a315d4d2d08116686983be197d6a7d6cfec35c70
Cl0udsoar <zhouyunxiang@vip.qq.com>
2022-09-23 09:57:16 +0800

    Update yangyang.js

--- yangyang.js
+++ yangyang.js
@@ -2,7 +2,7 @@ var body = $response.body;
 var a=JSON.parse(body);
 var b=a.data.map_seed;
 
-$notification.post("cloudsoartest","取:",b);
+$notification.post("cloudsoartest","潘月月是傻子:",b);
 var obj = JSON.parse(`{"err_code":0,"err_msg":"","data":{"map_md5":["046ef1bab26e5b9bfe2473ded237b572","046ef1bab26e5b9bfe2473ded237b572"],"map_seed":[${b}]}}`);
 
 body = JSON.stringify(obj);
