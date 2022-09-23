40e807d4b7ffb6923563d547bf9b5eff59462f75
Cl0udsoar <zhouyunxiang@vip.qq.com>
2022-09-23 08:16:43 +0800

    Update yangyang.js
    更新

--- yangyang.js
+++ yangyang.js
@@ -1,5 +1,6 @@
 var body = $response.body;
-var obj = JSON.parse(`{"err_code":0,"err_msg":"","data":{"map_md5":["046ef1bab26e5b9bfe2473ded237b572","046ef1bab26e5b9bfe2473ded237b572"],"map_seed":[3042065807,873076577,3555997730,3291121329]}}`);
+var a=JSON.parse(body);
+var obj = JSON.parse(`{"err_code":0,"err_msg":"","data":{"map_md5":["046ef1bab26e5b9bfe2473ded237b572","046ef1bab26e5b9bfe2473ded237b572"],"map_seed":${a.data.map_seed}}}`);
 
 body = JSON.stringify(obj);
 
