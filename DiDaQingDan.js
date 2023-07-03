a053b2b3c061f34a82fa1aeccc849e397ff3c38d
Cl0udsoar <zhouyunxiang@vip.qq.com>
2023-07-03 14:25:48 +0800

    Update DiDaQingDan.js

--- DiDaQingDan.js
+++ DiDaQingDan.js
@@ -1,16 +1,16 @@
-var _0xf36b=[
-    "body",               // "\x62\x6F\x64\x79"
-    "parse",              // "\x70\x61\x72\x73\x65"
-    "proEndDate",         // "\x70\x72\x6F\x45\x6E\x64\x44\x61\x74\x65"
-    "9999-08-08T00:00:00.000+0000", // 替换这里为新的日期
-    "needSubscribe",      // "\x6E\x65\x65\x64\x53\x75\x62\x73\x63\x72\x69\x62\x65"
-    "pro",                // "\x70\x72\x6F"
-    "stringify"           // "\x73\x74\x72\x69\x6E\x67\x69\x66\x79"
+var vvip=[
+    "body",              
+    "parse",            
+    "proEndDate",       
+    "8888-08-08T00:00:00.000+0000", 
+    "needSubscribe",     
+    "pro",             
+    "stringify"
 ];
-var body=$response[_0xf36b[0]];
-var obj=JSON[_0xf36b[1]](body);
-obj[_0xf36b[2]]= _0xf36b[3];
-obj[_0xf36b[4]]= false;
-obj[_0xf36b[5]]= true;
-body= JSON[_0xf36b[6]](obj);
+var body=$response[vvip[0]];
+var obj=JSON[vvip[1]](body);
+obj[vvip[2]]= vvip[3];
+obj[vvip[4]]= false;
+obj[vvip[5]]= true;
+body= JSON[vvip[6]](obj);
 $done({body})
