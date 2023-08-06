5661d9c2fa3cf66479dffa493151378ffceb490c
Cl0udsoar <zhouyunxiang@vip.qq.com>
2023-08-06 08:08:35 +0800

    Create WenZhenBaZiProCrack.js

---
+++ js/WenZhenBaZiProCrack.js
@@ -0,0 +1,2 @@
+var wzcreak={"code":-1,"data":{"vipTipsType":0,"vipLevel":3,"adtype":0,"expires":"9999-12-31 23:59:59"},"message":"","validateKey":"","isSuccess":true};
+$done({body:JSON.stringify(wzcreak)});
