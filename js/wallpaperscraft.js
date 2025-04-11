b7b4699d14c6c2f187ddc59a86b958dd4fc25370
Cl0udsoar <18359634+Cl0udsoar@users.noreply.github.com>
2025-04-11 14:44:40 +0800

    Update wallpaperscraft.js

--- js/wallpaperscraft.js
+++ js/wallpaperscraft.js
@@ -1,13 +1,11 @@
-// Wallcraft Pro Unlock for Surge
+// unlock-wallpaperscraft.js
 
-let obj = JSON.parse($response.body);
+let body = $response.body;
+let obj = JSON.parse(body);
 
-// 遍历所有 items 项，强制设置 is_active = true
-for (let key in obj.items) {
-  if (obj.items.hasOwnProperty(key)) {
+if (obj?.items) {
+  for (let key in obj.items) {
     obj.items[key].is_active = true;
-
-    // 如果存在 introductory_price 标志，也设置为已使用
     if (obj.items[key].hasOwnProperty("already_used_introductory_price")) {
       obj.items[key].already_used_introductory_price = true;
     }
