eabcf25c9c2f1b517edd85ca29933f53d420cbe8
Cl0udsoar <18359634+Cl0udsoar@users.noreply.github.com>
2025-04-11 14:40:49 +0800

    Update wallpaperscraft.js

--- js/wallpaperscraft.js
+++ js/wallpaperscraft.js
@@ -1,15 +1,17 @@
-let body = $response.body;
-let json = JSON.parse(body);
+// Wallcraft Pro Unlock for Surge
 
-const NON_CONSUMABLE_ID = "nonconsumable";
+let obj = JSON.parse($response.body);
 
-// 遍历所有订阅项目，将特定订阅标记为已激活
-if (json.subscriptions) {
-  for (let key in json.subscriptions) {
-    if (json.subscriptions[key].product_id === NON_CONSUMABLE_ID) {
-      json.subscriptions[key].is_active = true;
+// 遍历所有 items 项，强制设置 is_active = true
+for (let key in obj.items) {
+  if (obj.items.hasOwnProperty(key)) {
+    obj.items[key].is_active = true;
+
+    // 如果存在 introductory_price 标志，也设置为已使用
+    if (obj.items[key].hasOwnProperty("already_used_introductory_price")) {
+      obj.items[key].already_used_introductory_price = true;
     }
   }
 }
 
-$done({ body: JSON.stringify(json) });
+$done({ body: JSON.stringify(obj) });
