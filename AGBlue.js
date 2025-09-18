c1a95197869d9a171558d339b1a48b12cf80404d
Cl0udsoar <18359634+Cl0udsoar@users.noreply.github.com>
2025-09-18 14:23:49 +0800

    Update AGBlue.js

--- AGBlue.js
+++ AGBlue.js
@@ -5,7 +5,7 @@ try {
   body = {};
 }
 // 把 message 和 status_code 改成成功的值（只改 body）
-body.message = "ok";
-body.status_code = 6;
+body.message = "";
+body.status_code = 200;
 
 $done({body: JSON.stringify(body)});
